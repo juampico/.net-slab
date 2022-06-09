ef0034cbb7f6e49a2b613aac5004448134609d38
juampico <jcorrea@alumnos.exa.unicen.edu.ar>
2022-06-08 22:08:57 -0300

    TP4 - Agrego Unit Test

---
+++ Slab.Net.EF/Slab.Net.EF.LogicTests/CustomersLogicTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Slab.Net.EF.Commons.Exceptions;
+using Slab.Net.EF.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slab.Net.EF.Logic.Tests
+{
+    [TestClass()]
+    public class CustomersLogicTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void RemoveTest()
+        {
+            string toRemoveId = "ACSMZX";
+            CustomersLogic customersLogic = new CustomersLogic();
+            customersLogic.Remove(toRemoveId);
+        }
+
+    }
+}
---
+++ Slab.Net.EF/Slab.Net.EF.LogicTests/ShippersLogicTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Slab.Net.EF.Commons.Exceptions;
+using Slab.Net.EF.Entities;
+using Slab.Net.EF.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slab.Net.EF.Logic.Tests
+{
+    [TestClass()]
+    public class ShippersLogicTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(MaxLengthExceededException))]
+        public void AddTest()
+        {
+            Shippers newShipper = new Shippers
+            {
+                CompanyName = "NUEVO TRANSPORTISTA",
+                Phone = "(5099) - 249 - 431 - 429 - 562 - 122 - 130"
+            };
+            ShippersLogic shippersLogic = new ShippersLogic();
+            shippersLogic.Add(newShipper);
+        }
+    }
+}
